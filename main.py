1b003e958677da9a2d5fa06e92521d95c59d2fb2
tiny_mauss <105206479+inhat-d@users.noreply.github.com>
2024-09-27 20:51:48 +0300

    Added coments
    Me dumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 ### Info
-# Sorry for this strange strokes and maybe some mistakes, now it is 11:32 PM. I'm need a nap :(
+# Sorry for this strange strokes and maybe some mistakes, now it is 11:32 PM. I need a nap :(
 #
 # Are you watched "Infinite Train" serial? No? SO WATCH - IT'S SO BEAUTIFULKK. Khem... Nevermind...
 # This project is replica of OneOne, the double-minded robot. 
@@ -9,6 +9,9 @@
 ###
 
 
+
+### The Import Part.
+
 from openai import OpenAI
 
 import minds.theOneHappy as THO
@@ -17,22 +20,35 @@ import minds.theOneSad as TSO
 import random as rn
 import time as tm
 
+###
+
+
+
+### We need to chose!!!
+
 ask = input("Ask OneOne something > ")
 OneOne = rn.randint(1, 3)
 
-if OneOne == 1:
+###
+
+
+
+### The "If-Then" Part.
+
+if OneOne == 1:    #TheHappyOne
     print("God of randomnes desided that theHappyOne will answer you.")
     tm.sleep(3)
     THO.callhappy(prompt=ask)
 
-elif OneOne == 2:
+elif OneOne == 2:    #TheSadOne
     print("God of randomnes desided that theSadOne will answer you.")
     tm.sleep(3)
     TSO.callsad(prompt=ask)
 
-elif OneOne == 3:
+elif OneOne == 3:    #Both
     print("You extremly lucky, and both minds of OneOne will answer you")
     tm.sleep(3)
+    
     print("")
     print("TheHappyOne:")
     THO.callhappy(prompt=ask)
@@ -42,6 +58,11 @@ elif OneOne == 3:
     print("TheSadOne:")
     TSO.callsad(prompt=ask)
 
+###
+
+
+
+### Bye!
 
 for x in range(1, 4):
     print("")
